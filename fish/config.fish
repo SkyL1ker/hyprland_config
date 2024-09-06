ed55aef7b598b328cc60909494996f2bd2980eab
Maksym Tarasenko <47609174+SkyL1ker@users.noreply.github.com>
2024-09-06 22:40:00 +0000

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_greeting
+    neofetch
+    echo 'В глазах огонь,в душе аллах, козу трах трах, в метро бабах. '
+end
